a6c76c900f6cf42a325ce675077804b5ef4180b0
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-27 15:39:45 +0800

    Refactored azure credential setup and teardown scripts, added repository name variable, and updated credentials-params.yaml file.

---
+++ .github/resource/azure-credential-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Copyright (c) IBM Corporation.
+# Copyright (c) Microsoft Corporation.
+
+set -Eeuo pipefail
+
+echo "Execute azure-credential-setup.sh - Start------------------------------------------"
+
+## Create Azure Credentials
+AZURE_CREDENTIALS_SP_NAME="sp-${REPO_NAME}-$(date +%s)"
+echo "Creating Azure Service Principal with name: $AZURE_CREDENTIALS_SP_NAME"
+AZURE_CREDENTIALS=$(az ad sp create-for-rbac --name "$AZURE_CREDENTIALS_SP_NAME" --sdk-auth)
+
+## Set the Azure Credentials as a secret in the repository
+gh secret set "AZURE_CREDENTIALS" -b"${AZURE_CREDENTIALS}"
+gh variable set "AZURE_CREDENTIALS_SP_NAME" -b"${AZURE_CREDENTIALS_SP_NAME}"
+
+echo "Execute azure-credential-setup.sh - End--------------------------------------------"
