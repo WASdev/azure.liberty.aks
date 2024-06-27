8e4821ef824fca4e03dfe5919f5164e000d19283
zhihaoguo <zhihaoguo@microsoft.com>
2024-06-27 15:58:09 +0800

    Updated azure-credential-setup.sh to include Azure subscription ID and set role as contributor with specific scope.

--- .github/resource/azure-credential-setup.sh
+++ .github/resource/azure-credential-setup.sh
@@ -9,7 +9,8 @@ echo "Execute azure-credential-setup.sh - Start---------------------------------
 ## Create Azure Credentials
 AZURE_CREDENTIALS_SP_NAME="sp-${REPO_NAME}-$(date +%s)"
 echo "Creating Azure Service Principal with name: $AZURE_CREDENTIALS_SP_NAME"
-AZURE_CREDENTIALS=$(az ad sp create-for-rbac --name "$AZURE_CREDENTIALS_SP_NAME" --sdk-auth)
+AZURE_SUBSCRIPTION_ID=$(az account show --query id -o tsv| tr -d '\r\n')
+AZURE_CREDENTIALS=$(az ad sp create-for-rbac --name "$AZURE_CREDENTIALS_SP_NAME" --role contributor --scopes /subscriptions/"$AZURE_SUBSCRIPTION_ID" --sdk-auth)
 
 ## Set the Azure Credentials as a secret in the repository
 gh secret set "AZURE_CREDENTIALS" -b"${AZURE_CREDENTIALS}"
