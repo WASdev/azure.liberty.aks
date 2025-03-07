ba63008801902d791216215a9d01b4570e92eb0d
zhihaoguo <zhihaoguo@microsoft.com>
2025-03-07 11:20:48 +0800

    update azure credential teardown script to specify repository context for variable retrieval

--- .github/resource/azure-credential-teardown.sh
+++ .github/resource/azure-credential-teardown.sh
@@ -8,7 +8,7 @@ CURRENT_FILE_NAME="azure-credential-teardown.sh"
 echo "Execute $CURRENT_FILE_NAME - Start------------------------------------------"
 
 gh secret --repo $(gh repo set-default --view) delete "AZURE_CREDENTIALS"
-AZURE_CREDENTIALS_SP_NAME=$(gh variable get "AZURE_CREDENTIALS_SP_NAME")
+AZURE_CREDENTIALS_SP_NAME=$(gh variable --repo $(gh repo set-default --view) get "AZURE_CREDENTIALS_SP_NAME")
 az ad app delete --id $(az ad sp list --display-name "$AZURE_CREDENTIALS_SP_NAME" --query '[].appId' -o tsv| tr -d '\r\n')
 gh variable --repo $(gh repo set-default --view) delete "AZURE_CREDENTIALS_SP_NAME"
 
